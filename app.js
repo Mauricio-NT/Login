ce14e79c3346a5f31c014ea3e7362499a22dbdc7
Maurício <mauricio.sbneto@gmail.com>
2023-10-30 09:02:49 -0300

    update document

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const fundo = document.querySelector(".fundo");
 const loginlink = document.querySelector(".login-link");
-const registrarlink = documento.querySelector(".registro-link");
+const registrarlink = document.querySelector(".registro-link");
 const btn = document.querySelector (".btn");
 const iconeEncerrar = document.querySelector (".icone-encerrar");
 
 registrarlink.addEventListener ("click", () =>{
     fundo.classList.add('active');
-    console.log("Botao clicado");
+    //console.log("Botao clicado");
 });
 
 loginlink.addEventListener("click", () =>{
     fundo.classList.remove('active');
-    console.log("Botao clicado");
+    //console.log("Botao clicado");
 });
 
 btn.addEventListener("click", () =>{
     fundo.classList.add('active-btn');
-    console.log("Botao clicado");
+    //console.log("Botao clicado");
 });
 
 iconeEncerrar.addEventListener("click", () =>{
     fundo.classList.remove ('active-btn');
-    console.log("Botao clicado");
+    //console.log("Botao clicado");
 });
